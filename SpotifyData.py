e015b099f53868df95c4e2c607a3690fce7e53d3
Addison Shaw <addison@xlab.me>
2016-02-25 18:06:46 -0600

    Added in AWS functionality so that I can use EMR for Hadoop and Spark and I can use S3 for storage. Added in SQL queries for warm-up section as well as my custom analysis of the data.

--- SpotifyData.py
+++ SpotifyData.py
@@ -17,7 +17,8 @@ sqlContext = SQLContext(sc)
 #######################################
 
 # Load in the first Spotify data sample.
-endSongSample = sc.textFile('data/end_song_sample.csv')
+# This is my AWS S3 bucket, but it can be any file system location.
+endSongSample = sc.textFile('s3://aws-logs-308764694576-us-west-2/end_song_sample.csv')
 songColumns = endSongSample.first()
 
 # Isolate the rows minus the header.
@@ -39,13 +40,10 @@ songFields[0].dataType = IntegerType()
 songSchema = StructType(songFields)
 
 # Parse our file (without the header) into a format that can be used by Spark's sqlContext.
-endSongSqlFormat = endSongNoHeader.map(lambda a: a.split(","))
-
-# Create our first data frame.
-endSongDataFrame = sqlContext.createDataFrame(endSongSqlFormat, songSchema)
+endSongDataFrame = endSongNoHeader.map(lambda a: a.split(",")).map(lambda p: (int(p[0]), p[1], p[2], p[3], p[4], p[5])).toDF(songSchema)
 
 # Load the second Spotify data sample.
-userData = sc.textFile('data/user_data_sample.csv')
+userData = sc.textFile('s3://aws-logs-308764694576-us-west-2/user_data_sample.csv')
 userColumns = userData.first()
 
 # Isolate rows.
@@ -66,10 +64,7 @@ userFields[3].dataType = IntegerType()
 userSchema = StructType(userFields)
 
 # Parse our file (without the header) into a format that can be used by Spark's sqlContext.
-userSqlFormat = userNoHeader.map(lambda a: a.split(","))
-
-# Create our second data frame.
-userDataFrame = sqlContext.createDataFrame(userSqlFormat, userSchema)
+userDataFrame = userNoHeader.map(lambda a: a.split(",")).map(lambda p: (p[0], p[1], p[2], int(p[3]), p[4])).toDF(userSchema)
 
 #######################################
 ###   SQL section
@@ -78,4 +73,29 @@ userDataFrame = sqlContext.createDataFrame(userSqlFormat, userSchema)
 # Register both dataframes as SQL tables
 endSongDataFrame.registerTempTable('end_song_data')
 userDataFrame.registerTempTable('user_data')
-sqlContext.sql("SELECT user_id, COUNT(*) FROM end_song_data GROUP BY user_id").show()
+
+#######################################
+###   SQL section - Warm up
+#######################################
+
+joinedTable = sqlContext.sql("""SELECT u.age_range AS age_range,
+                                u.country AS country,
+                                e.context AS context,
+                                e.product AS product,
+                                u.user_id AS user_id,
+                                u.gender AS gender,
+                                e.track_id AS track_id,
+                                e.ms_played AS ms_played
+                                FROM end_song_data AS e, user_data AS u
+                                WHERE u.user_id = e.user_id""")
+joinedTable.registerTempTable("joined_table")
+
+# This query will show the difference of listening times between genders.
+sqlContext.sql("SELECT gender, SUM(ms_played) AS total_time_played FROM joined_table GROUP BY gender").show()
+
+# These queries are meant to compare 'free' or 'open' product usage to 'premium' across all countries using Spotify.
+# Unpaid usage query:
+sqlContext.sql("SELECT sub.country, COUNT(sub.product) AS unpaid_users FROM (SELECT DISTINCT user_id, product, country FROM joined_table WHERE product = 'open' OR product = 'free') AS sub GROUP BY country ORDER BY unpaid_users DESC LIMIT 2").show()
+
+# Paid usage query:
+sqlContext.sql("SELECT sub.country, COUNT(sub.product) AS paid_users FROM (SELECT DISTINCT user_id, product, country FROM joined_table WHERE product = 'premium') AS sub GROUP BY country ORDER BY paid_users DESC LIMIT 2").show()
